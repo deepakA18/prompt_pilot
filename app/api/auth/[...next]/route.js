4d766e7e5647da73ddf41caee1b017bad292089f
deepakagashe18 <agashedeepak55@gmail.com>
2024-02-01 19:44:12 +0530

    env

--- app/api/auth/[...next]/route.js
+++ app/api/auth/[...next]/route.js
@@ -1,6 +1,9 @@
 import NextAuth  from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google"
 
+import {connectToDB} from "@utils/database"
+import User from "@models/user";
+
 
 const handler =  NextAuth[{
 
@@ -11,15 +14,32 @@ const handler =  NextAuth[{
         })
     ],
     async session({session}){
+        const sessionUser = await User.findOne({
+            email: session.user.email
+        })
 
+        session.user.id = sessionUser._id.toString();
+
+        return session;
     },
     async signIn({profile}){
         try {
-            
+            await connectToDB();
+
+            const user = await User.findOne({email: profile.email});
+
+            if(!user)
+            {
+                await User.create({
+                    email: profile.email,
+                    username: profile.name.replace(" "," "),
+                    image: profile.picture
+                })
+            }
         } catch (error) {
             
         }
     }
 }]
 
-export default handler;
+export {handler as GET, handler as POST};
